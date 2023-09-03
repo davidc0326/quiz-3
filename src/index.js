258004facc2b99531d5b1dc422f1dd6f171fcd62
David Cedillo <137354680+davidc0326@users.noreply.github.com>
2023-09-03 11:01:35 -0400

    switch quizz completado

--- src/index.js
+++ src/index.js
@@ -1,80 +1,235 @@
 /**
- * This quiz is to test your knowledge of Teranary Operator.
- *
- * 1. Create a repository by cloning: https://github.com/jorgecarmona/npm-project-template
- * 2. Follow the README for instructions
- * 3. When you finish the quiz, create a PR on you repository
- * 4. Submit to your lead
+ * Question 1
+ * Given a variable color with a value "red", use a switch statement to set a variable feeling to "passionate" for "red", "calm" for "blue", and "neutral" for other colors.
  */
+let color = "red";
+let feeling;
+
+switch (color) {
+  case "red":
+    feeling = "passionate";
+    break;
+  case "blue":
+    feeling = "calm";
+    break;
+  default:
+    feeling = "neutral";
+}
+
+console.log(feeling);
 
 /**
- * Question 1
- * Declare a variable isSunny with a value of true. Use a ternary operator to set a variable activity to "Go to the beach" if isSunny is true and "Stay indoors" otherwise.
- */
-const isSunny = true;
-let activity = isSunny ? "go to the beach" : "stay home";
-console.log(activity);
-/**
  * Question 2
- * Given a variable age = 20, use a ternary operator to determine if the age is greater than 18. If so, set a variable status to "Adult", otherwise set it to "Minor".
+ * Using a switch statement, check the value of the variable day (e.g., "Monday"). Return "Working day" for weekdays and "Weekend" for Saturday and Sunday.
  */
-let age = 20;
-let status = age > 18 ? "Aduld" : "Minor";
-console.log(status);
+let day = "Monday";
+let dayType;
+
+switch (day) {
+  case "Monday":
+  case "Tuesday":
+  case "Wednesday":
+  case "Thursday":
+  case "Friday":
+    dayType = "Working day";
+    break;
+  case "Saturday":
+  case "Sunday":
+    dayType = "Weekend";
+    break;
+  default:
+    dayType = "Invalid day";
+}
+
+console.log(dayType);
+
 /**
  * Question 3
- * Using a ternary operator, check if the length of the array [1, 2, 3, 4] is even. If true, set a variable isEvenLength to true, otherwise set it to false.
+ * Given a variable score of 85, set a grade: "A" for 90 and above, "B" for 80-89, "C" for 70-79, "D" for 60-69, and "F" for below 60.
  */
-const arrayLength = [1, 2, 3, 4];
-let isEvenLength = arrayLength % 2 === 3 ? true : false;
-console.log(isEvenLength);
+let score = 85;
+let grade;
+
+switch (true) {
+  case score >= 90:
+    grade = "A";
+    break;
+  case score >= 80:
+    grade = "B";
+    break;
+  case score >= 70:
+    grade = "C";
+    break;
+  case score >= 60:
+    grade = "D";
+    break;
+  default:
+    grade = "F";
+}
+
+console.log(grade);
+
 /**
  * Question 4
- * Given a variable score = 45, use a ternary operator to set a variable result to "Pass" if score is greater than or equal to 50 and "Fail" otherwise.
+ * Use a switch statement to determine if the variable fruit is a "berry". Set "berry" for strawberries, blueberries, and raspberries. "Not a berry" for other fruits.
  */
-let score = 45;
-let result = score >= 50 ? "Pass" : "fail";
-console.log(result);
+let fruit = "strawberries";
+let fruitType;
+
+switch (fruit) {
+  case "strawberries":
+  case "blueberries":
+  case "raspberries":
+    fruitType = "berry";
+    break;
+  default:
+    fruitType = "Not a berry";
+}
+
+console.log(fruitType);
+
 /**
  * Question 5
- * Use a ternary operator to determine whether the number 15 is even or odd. Set a variable parity to the result.
+ * Check the value of the variable month (e.g., "January") and return which quarter of the year it belongs to.
  */
-let number = 15;
-let parity = number % 2 === 0 ? "even" : "Odd";
-console.log(parity);
+let month = "January";
+let quarter;
+
+switch (month) {
+  case "January":
+  case "February":
+  case "March":
+    quarter = "Q1";
+    break;
+  case "April":
+  case "May":
+  case "June":
+    quarter = "Q2";
+    break;
+  case "July":
+  case "August":
+  case "September":
+    quarter = "Q3";
+    break;
+  case "October":
+  case "November":
+  case "December":
+    quarter = "Q4";
+    break;
+  default:
+    quarter = "Invalid month";
+}
+
+console.log(quarter);
+
 /**
  * Question 6
- * Given a variable temperature = 22, use a ternary operator to set a variable outfit to "Wear a jacket" if temperature is below 20 and "T-shirt is fine" otherwise.
+ * Use a switch statement to determine if a number is "small", "medium", or "large". Consider numbers 1-3 as "small", 4-6 as "medium", and 7-9 as "large".
  */
-let temperature = 22;
-let outfit = temperature < 20 ? "Wear a Jackect" : "T-shrit is fine";
-console.log(outfit);
+let number = 5;
+let size;
+
+switch (true) {
+  case number >= 1 && number <= 3:
+    size = "small";
+    break;
+  case number >= 4 && number <= 6:
+    size = "medium";
+    break;
+  case number >= 7 && number <= 9:
+    size = "large";
+    break;
+  default:
+    size = "Invalid number";
+}
+
+console.log(size);
+
 /**
  * Question 7
- * Using a ternary operator, determine if the first element of the array ["apple", "banana", "cherry"] is "apple". If true, set a variable isApple to true, otherwise set it to false.
+ * Determine the type of the variable pet (e.g., "dog"). Return "Canine" for a dog, "Feline" for a cat, and "Unknown" for other pets.
  */
-const fruits = ["apple", "banana", "cherry"];
-let isApple = fruits[0] === "apple" ? true : false;
-console.log(isApple);
+let pet = "dog";
+let petType;
+
+switch (pet) {
+  case "dog":
+    petType = "Canine";
+    break;
+  case "cat":
+    petType = "Feline";
+    break;
+  default:
+    petType = "Unknown";
+}
+
+console.log(petType);
+
 /**
  * Question 8
- * Given a variable height = 180, use a ternary operator to set a variable basketball to "You're tall enough to play" if height is greater than 175 and "Might be a bit short" otherwise.
+ * Given a variable transportMode with a value like "car", return "Fast" for "plane", "Medium" for "car", and "Slow" for "bicycle".
  */
-let height = 180;
-let basquetball =
-  height > 175 ? "you are tall enought to play" : "may be a little bit short";
-console.log(basquetball);
+let transportMode = "car";
+let speed;
+
+switch (transportMode) {
+  case "plane":
+    speed = "Fast";
+    break;
+  case "car":
+    speed = "Medium";
+    break;
+  case "bicycle":
+    speed = "Slow";
+    break;
+  default:
+    speed = "Unknown";
+}
+
+console.log(speed);
+
 /**
  * Question 9
- * Given a variable day = "Sunday", use a ternary operator to set a variable workday to "Weekend" if day is either "Saturday" or "Sunday" and "Workday" otherwise.
+ * For a variable direction with values like "N", determine the full direction name. "N" for "North", "S" for "South", etc.
  */
-const day = "sunday";
-let workday = day === "Saturday" || day === "sunday" ? "weekend" : "workday";
-console.log(workday);
+let direction = "N";
+let fullDirection;
+
+switch (direction) {
+  case "N":
+    fullDirection = "North";
+    break;
+  case "S":
+    fullDirection = "South";
+    break;
+  case "E":
+    fullDirection = "East";
+    break;
+  case "W":
+    fullDirection = "West";
+    break;
+  default:
+    fullDirection = "Unknown direction";
+}
+
+console.log(fullDirection);
+
 /**
  * Question 10
- * Use a ternary operator to determine if the string "hello" is of length 5. If true, set a variable isLengthFive to true, otherwise set it to false.
+ * Determine the type of drink based on the variable drink. "Water" for "H2O", "Coffee" for "C8H10N4O2", and "Unknown" for other values.
  */
-let str = "hello";
-let isLengthFive = str.length === 5 ? true : false;
-console.log(isLengthFive);
+let drink = "H2O";
+let drinkType;
+
+switch (drink) {
+  case "H2O":
+    drinkType = "Water";
+    break;
+  case "C8H10N4O2":
+    drinkType = "Coffee";
+    break;
+  default:
+    drinkType = "Unknown";
+}
+
+console.log(drinkType);
