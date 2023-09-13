97621bd04e4c3c37bca83bdd0ac07e3b2374def0
David Cedillo <137354680+davidc0326@users.noreply.github.com>
2023-09-12 21:19:50 -0400

    se completo el quiz for-of-loop

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // Question 1: Loop through the array and print the names of the Apostles
 const apostles = ["Peter", "James", "John", "Andrew", "Philip"];
 for (const apostle of apostles) {
-  console.log("[Question #1:]", apostle);
+  console.log("[Question #1]:", apostle);
 }
 
 // Question 2: Calculate the sum of the ages of the Patriarchs
@@ -10,7 +10,7 @@ let sumOfAges = 0;
 for (const age of patriarchAges) {
   sumOfAges += age;
 }
-console.log("[Question #2:]", sumOfAges);
+console.log("[Question #2]:", sumOfAges);
 
 // Question 3: Print the plagues of Egypt
 const plagues = ["Frogs", "Locusts", "Hail"];
@@ -21,7 +21,7 @@ for (const plague of plagues) {
 // Question 4: Convert Bible books to uppercase
 const books = ["Genesis", "Exodus", "Leviticus"];
 for (const book of books) {
-  console.log("[Question #4:]", book.toUpperCase());
+  console.log("[Question #4]:", book.toUpperCase());
 }
 
 // Question 5: Count the number of times 'apple' appears in the array
@@ -32,7 +32,7 @@ for (const fruit of fruitsInEden) {
     appleCount++;
   }
 }
-console.log("[Question #5:]", appleCount);
+console.log("[Question #5]:", appleCount);
 
 // Question 6: Multiply all elements in the array by 2
 const davidStones = [1, 2, 3];
@@ -40,7 +40,7 @@ const multipliedStones = [];
 for (const stone of davidStones) {
   multipliedStones.push(stone * 2);
 }
-console.log("[Question #6:]", multipliedStones);
+console.log("[Question #6]:", multipliedStones);
 
 // Question 7: Reverse the string
 const nameOfGod = "Yahweh";
@@ -48,23 +48,23 @@ let reversedName = "";
 for (const letter of nameOfGod) {
   reversedName = letter + reversedName;
 }
-console.log("[Question #7:]", reversedName);
+console.log("[Question #7]:", reversedName);
 
 // Question 8: Create a sentence by adding spaces to words
 const peaceBeUponYou = ["Peace", "be", "upon", "you"];
 const sentence = peaceBeUponYou.join(" ");
-console.log("[Question #8:]", sentence);
+console.log("[Question #8]:", sentence);
 
 // Question 9: Print out every other element from the array
 const daysOfCreation = ["Day1", "Day2", "Day3", "Day4", "Day5", "Day6"];
 for (let i = 0; i < daysOfCreation.length; i += 2) {
-  console.log("[Question #9:]", daysOfCreation[i]);
+  console.log("[Question #9]:", daysOfCreation[i]);
 }
 
 // Question 10: Concatenate all the strings in the array
 const attributesOfGod = ["Omnipotent", "Omnipresent", "Omniscient"];
 const concatenatedAttributes = attributesOfGod.join("");
-console.log("[Question #10:]", concatenatedAttributes);
+console.log("[Question #10]:", concatenatedAttributes);
 
 // Question 11: Create a new array with only the numbers greater than 3
 const biblicalNumbers = [1, 3, 7, 12];
@@ -74,7 +74,7 @@ for (const number of biblicalNumbers) {
     greaterThan3.push(number);
   }
 }
-console.log("[Question #11:]", greaterThan3);
+console.log("[Question #11]:", greaterThan3);
 
 // Question 12: Calculate the product of all the elements in the array
 const disciplesAges = [30, 25, 40, 50];
@@ -82,7 +82,7 @@ let productOfAges = 1;
 for (const age of disciplesAges) {
   productOfAges *= age;
 }
-console.log("[Question #12:]", productOfAges);
+console.log("[Question #12]:", productOfAges);
 
 // Question 13: Replace 'Goliath' with 'David' in the array
 const combatants = ["Goliath", "Soldier1", "Soldier2"];
@@ -91,7 +91,7 @@ for (let i = 0; i < combatants.length; i++) {
     combatants[i] = "David";
   }
 }
-console.log("[Question #13:]", combatants);
+console.log("[Question #13]:", combatants);
 
 // Question 14: Print the square of each element in the array
 const squaresOfNumbers = [1, 2, 3, 4];
@@ -108,12 +108,12 @@ for (const letter of theWord.toLowerCase()) {
     vowelCount++;
   }
 }
-console.log("[Question #15:]", vowelCount);
+console.log("[Question #15]:", vowelCount);
 
 // Question 16: Print the elements that are divisible by 5
 const numbersFromBible = [5, 10, 15, 20, 25];
 for (const number of numbersFromBible) {
-  console.log("[Question #16:]", number);
+  console.log("[Question #16]:", number);
 }
 
 // Question 17: Create a new string where the first letter of each word is capitalized
@@ -132,7 +132,7 @@ const doubledElements = [];
 for (const element of loavesAndFishes) {
   doubledElements.push(element * 2);
 }
-console.log("[Question #18:]", doubledElements);
+console.log("[Question #18]:", doubledElements);
 
 // Question 19: Count the number of elements that are equal to 'manna'
 const foodInDesert = ["manna", "quail", "manna", "manna"];
@@ -142,7 +142,7 @@ for (const food of foodInDesert) {
     mannaCount++;
   }
 }
-console.log("[Question #19:]", mannaCount);
+console.log("[Question #19]:", mannaCount);
 
 // Question 20: Create a new array by picking every 3rd element from the original array
 const theCommandments = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
@@ -150,4 +150,4 @@ const everyThirdElement = [];
 for (let i = 2; i < theCommandments.length; i += 3) {
   everyThirdElement.push(theCommandments[i]);
 }
-console.log("[Question #20:]", everyThirdElement);
+console.log("[Question #20]:", everyThirdElement);
