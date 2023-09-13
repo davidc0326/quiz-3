05749128a7b514eceb3ec4347cfa25f780162fc8
David Cedillo <137354680+davidc0326@users.noreply.github.com>
2023-09-12 21:14:39 -0400

    se completo el quiz for-of-loop

--- src/index.js
+++ src/index.js
@@ -1,181 +1,153 @@
-/**
- * Question 1
- * Using a while loop, sum all numbers from 1 to 10 and store the result in a variable called sum.
- */
-
-let sum = 0;
-let i = 1;
-
-while (i <= 10) {
-  sum += i;
-  i++;
+// Question 1: Loop through the array and print the names of the Apostles
+const apostles = ["Peter", "James", "John", "Andrew", "Philip"];
+for (const apostle of apostles) {
+  console.log("[Question #1:]", apostle);
 }
 
-console.log("Question #1: Sum of numbers from 1 to 10 is " + sum);
+// Question 2: Calculate the sum of the ages of the Patriarchs
+const patriarchAges = [930, 912, 905, 895];
+let sumOfAges = 0;
+for (const age of patriarchAges) {
+  sumOfAges += age;
+}
+console.log("[Question #2:]", sumOfAges);
 
-/**
- * Question 2
- * Using a while loop, create a string of numbers from 5 to 15 separated by commas, e.g., "5,6,7,...,15".
- */
-
-let numbersString = "";
-let j = 5;
-
-while (j <= 15) {
-  numbersString += j;
-  if (j < 15) {
-    numbersString += ",";
-  }
-  j++;
+// Question 3: Print the plagues of Egypt
+const plagues = ["Frogs", "Locusts", "Hail"];
+for (const plague of plagues) {
+  console.log("[Question #3:]", plague);
 }
 
-console.log("Question 2: String of numbers from 5 to 15 is " + numbersString);
-
-// /**
-//  * Question 3
-//  * Print all even numbers between 10 and 30 using a while loop.
-//  */
-
-// Inicializamos una cadena vacía para almacenar los números pares
-let evenNumbersString = "";
-
-// Inicializamos la variable con el primer número par en el rango
-let evenNumber2 = 10;
-
-// Usamos un bucle while para recorrer los números pares entre 10 y 30
-while (evenNumber2 <= 30) {
-  // Verificamos si el número actual es par
-  if (evenNumber2 % 2 === 0) {
-    // Agregamos el número par a la cadena con una coma y un espacio
-    evenNumbersString += evenNumber2;
-
-    // Verificamos si este no es el último número par para agregar una coma
-    if (evenNumber2 < 30 - 1) {
-      evenNumbersString += ", ";
-    }
-  }
-
-  // Incrementamos el número en 2 para pasar al siguiente número par
-  evenNumber2 += 2;
+// Question 4: Convert Bible books to uppercase
+const books = ["Genesis", "Exodus", "Leviticus"];
+for (const book of books) {
+  console.log("[Question #4:]", book.toUpperCase());
 }
 
-// Imprimimos la cadena que contiene los números pares
-console.log("question #3:", evenNumbersString);
-/**
- * Question 4
- * Determine the smallest power of 2 greater than 1000 using a while loop.
- */
-
-let powerOfTwo = 1;
-let exponent = 0;
-
-while (powerOfTwo <= 1000) {
-  powerOfTwo = Math.pow(2, exponent);
-  exponent++;
-}
-
-console.log(
-  "Question 4: Smallest power of 2 greater than 1000 is 2^" + (exponent - 1)
-);
-
-/**
- * Question 5
- * Reverse a string "HelloWorld" using a while loop.
- */
-
-let originalString = "HelloWorld";
-let reversedString = "";
-let k = originalString.length - 1;
-
-while (k >= 0) {
-  reversedString += originalString[k];
-  k--;
-}
-
-console.log("Question 5: Reversed string is " + reversedString);
-
-/**
- * Question 6
- * Using a while loop, count how many times the letter 'a' appears in the string "banana".
- */
-
-let inputString = "banana";
-let letterToCount = "a";
-let count = 0;
-let l = 0;
-
-while (l < inputString.length) {
-  if (inputString[l] === letterToCount) {
-    count++;
-  }
-  l++;
-}
-
-console.log(
-  "Question 6: Count of '" + letterToCount + "' in 'banana' is " + count
-);
-
-/**
- * Question 7
- * Using a while loop, compute the factorial of 5.
- */
-
-let num = 5;
-let factorial = 1;
-let m = 1;
-
-while (m <= num) {
-  factorial *= m;
-  m++;
-}
-
-console.log("Question 7: Factorial of 5 is " + factorial);
-
-/**
- * Question 8
- * Print all numbers from 100 to 80 in descending order using a while loop.
- */
-
-let n = 100;
-
-console.log("Question 8: Numbers from 100 to 80 in descending order:");
-
-while (n >= 80) {
-  console.log(n);
-  n--;
-}
-
-/**
- * Question 9
- * Using a while loop, find the greatest common divisor (GCD) of two numbers, 56 and 98.
- */
-
-let number1 = 56;
-let number2 = 98;
-
-while (number1 !== number2) {
-  if (number1 > number2) {
-    number1 -= number2;
-  } else {
-    number2 -= number1;
+// Question 5: Count the number of times 'apple' appears in the array
+const fruitsInEden = ["apple", "pear", "apple", "grape"];
+let appleCount = 0;
+for (const fruit of fruitsInEden) {
+  if (fruit === "apple") {
+    appleCount++;
   }
 }
+console.log("[Question #5:]", appleCount);
 
-console.log("Question 9: GCD of 56 and 98 is " + number1);
+// Question 6: Multiply all elements in the array by 2
+const davidStones = [1, 2, 3];
+const multipliedStones = [];
+for (const stone of davidStones) {
+  multipliedStones.push(stone * 2);
+}
+console.log("[Question #6:]", multipliedStones);
 
-/**
- * Question 10
- * Calculate the sum of all odd numbers between 1 and 50 using a while loop.
- */
+// Question 7: Reverse the string
+const nameOfGod = "Yahweh";
+let reversedName = "";
+for (const letter of nameOfGod) {
+  reversedName = letter + reversedName;
+}
+console.log("[Question #7:]", reversedName);
 
-let oddSum = 0;
-let p = 1;
+// Question 8: Create a sentence by adding spaces to words
+const peaceBeUponYou = ["Peace", "be", "upon", "you"];
+const sentence = peaceBeUponYou.join(" ");
+console.log("[Question #8:]", sentence);
 
-while (p <= 50) {
-  if (p % 2 !== 0) {
-    oddSum += p;
-  }
-  p++;
+// Question 9: Print out every other element from the array
+const daysOfCreation = ["Day1", "Day2", "Day3", "Day4", "Day5", "Day6"];
+for (let i = 0; i < daysOfCreation.length; i += 2) {
+  console.log("[Question #9:]", daysOfCreation[i]);
 }
 
-console.log("Question 10: Sum of odd numbers between 1 and 50 is " + oddSum);
+// Question 10: Concatenate all the strings in the array
+const attributesOfGod = ["Omnipotent", "Omnipresent", "Omniscient"];
+const concatenatedAttributes = attributesOfGod.join("");
+console.log("[Question #10:]", concatenatedAttributes);
+
+// Question 11: Create a new array with only the numbers greater than 3
+const biblicalNumbers = [1, 3, 7, 12];
+const greaterThan3 = [];
+for (const number of biblicalNumbers) {
+  if (number > 3) {
+    greaterThan3.push(number);
+  }
+}
+console.log("[Question #11:]", greaterThan3);
+
+// Question 12: Calculate the product of all the elements in the array
+const disciplesAges = [30, 25, 40, 50];
+let productOfAges = 1;
+for (const age of disciplesAges) {
+  productOfAges *= age;
+}
+console.log("[Question #12:]", productOfAges);
+
+// Question 13: Replace 'Goliath' with 'David' in the array
+const combatants = ["Goliath", "Soldier1", "Soldier2"];
+for (let i = 0; i < combatants.length; i++) {
+  if (combatants[i] === "Goliath") {
+    combatants[i] = "David";
+  }
+}
+console.log("[Question #13:]", combatants);
+
+// Question 14: Print the square of each element in the array
+const squaresOfNumbers = [1, 2, 3, 4];
+for (const number of squaresOfNumbers) {
+  console.log("[Question #14:]", number * number);
+}
+
+// Question 15: Count the number of vowels in the string
+const theWord = "Bible";
+let vowelCount = 0;
+const vowels = ["a", "e", "i", "o", "u"];
+for (const letter of theWord.toLowerCase()) {
+  if (vowels.includes(letter)) {
+    vowelCount++;
+  }
+}
+console.log("[Question #15:]", vowelCount);
+
+// Question 16: Print the elements that are divisible by 5
+const numbersFromBible = [5, 10, 15, 20, 25];
+for (const number of numbersFromBible) {
+  console.log("[Question #16:]", number);
+}
+
+// Question 17: Create a new string where the first letter of each word is capitalized
+const phrase = "in the beginning";
+const words = phrase.split(" ");
+let capitalizedPhrase = "";
+for (const word of words) {
+  capitalizedPhrase += word.charAt(0).toUpperCase() + word.slice(1) + " ";
+}
+capitalizedPhrase = capitalizedPhrase.trim();
+console.log("[Question #17:]", capitalizedPhrase);
+
+// Question 18: Create a new array where each element is double the original
+const loavesAndFishes = [2, 5];
+const doubledElements = [];
+for (const element of loavesAndFishes) {
+  doubledElements.push(element * 2);
+}
+console.log("[Question #18:]", doubledElements);
+
+// Question 19: Count the number of elements that are equal to 'manna'
+const foodInDesert = ["manna", "quail", "manna", "manna"];
+let mannaCount = 0;
+for (const food of foodInDesert) {
+  if (food === "manna") {
+    mannaCount++;
+  }
+}
+console.log("[Question #19:]", mannaCount);
+
+// Question 20: Create a new array by picking every 3rd element from the original array
+const theCommandments = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const everyThirdElement = [];
+for (let i = 2; i < theCommandments.length; i += 3) {
+  everyThirdElement.push(theCommandments[i]);
+}
+console.log("[Question #20:]", everyThirdElement);
